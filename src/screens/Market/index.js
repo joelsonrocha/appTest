22c290c85d970f7fd87dec0e49472eff0bda4325
Joao Dias <joao.dias@midwayfinanceira.com.br>
2020-03-05 21:09:05 -0300

    adding map

--- src/screens/Market/index.js
+++ src/screens/Market/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import MapView from 'react-native-maps';
 import Header from '../../components/Header';
 
 export default function Market({ navigation }) {
   return (
-    <SafeAreaView>
-      <Header title="Market" navigation={navigation} showMenu={true}></Header>
+    <SafeAreaView style={styles.content}>
+      <Header title="Market" navigation={navigation} showMenu />
+      <MapView
+        style={styles.bgMap}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1
+  },
+  bgMap: {
+    flex: 1
+  }
+});
