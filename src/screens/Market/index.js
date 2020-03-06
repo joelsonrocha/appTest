4de445c3ab501dbbb050fa1abc73e3d4431d429c
Joao Dias <joao.dias@midwayfinanceira.com.br>
2020-03-06 02:37:53 -0300

    adding geolocalization

--- src/screens/Market/index.js
+++ src/screens/Market/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, SafeAreaView, Alert } from 'react-native';
 import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 import Header from '../../components/Header';
 
 export default function Market({ navigation }) {
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: -23.52510662325689,
+    longitude: -46.68052477139523,
+    latitudeDelta: 0.007200766742091247,
+    longitudeDelta: 0.0044417387748012516
+  });
+
+  useEffect(() => {
+    const findCoordinates = () => {
+      Geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation({
+            latitude,
+            longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          });
+        },
+        error => Alert.alert(error.message),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      );
+    };
+    findCoordinates();
+  }, []);
+
   return (
     <SafeAreaView style={styles.content}>
       <Header title="Market" navigation={navigation} showMenu />
-      <MapView
-        style={styles.bgMap}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
+      <MapView style={styles.bgMap} region={currentLocation} />
     </SafeAreaView>
   );
 }
